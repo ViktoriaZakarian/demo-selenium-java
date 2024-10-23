1e3e15088da03061f910eb4009bdcd906ba39c6b
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-23 23:14:08 +0300

    add test to validate login form with any email

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -18,4 +18,19 @@ public class LoginTest {
         WebElement buttonSignInWebElement = webDriver.findElement(buttonSignInBy);
         buttonSignInWebElement.click();
     }
+
+    public void test2() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://secure.login.gov/");
+
+        String inputEmailXpath = "/html/body/main/div/form/lg-validated-field/div/input";
+        By inputEmailBy = By.xpath(inputEmailXpath);
+        WebElement inputEmailWebElement = webDriver.findElement(inputEmailBy);
+        inputEmailWebElement.sendKeys("testA@test.com");
+
+        String buttonSignInXpath = "/html/body/main/div/form/lg-submit-button/button";
+        By buttonSignInBy = By.xpath(buttonSignInXpath);
+        WebElement buttonSignInWebElement = webDriver.findElement(buttonSignInBy);
+        buttonSignInWebElement.click();
+    }
 }
