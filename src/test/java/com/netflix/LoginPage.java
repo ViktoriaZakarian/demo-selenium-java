f9f283491e5751949b827de2ec23b0a5d8476fb7
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 19:40:36 +0300

    refactor LoginPage add methods clickButtonEntrance and sendKeysInputEmail

--- src/test/java/com/netflix/LoginPage.java
+++ src/test/java/com/netflix/LoginPage.java
@@ -1,5 +1,24 @@
 package com.netflix;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
 
+    private final WebDriver webDriver;
+
+    public LoginPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void clickButtonEntrance() {
+        WebElement buttonEntranceWebElement = webDriver.findElement(By.xpath(LoginXpath.BUTTON_ENTRANCE_XPATH));
+        buttonEntranceWebElement.click();
+    }
+
+    public void sendKeysInputEmail(String email) {
+        WebElement inputEmailWebElement = webDriver.findElement(By.xpath(LoginXpath.INPUT_EMAIL_XPATH));
+        inputEmailWebElement.sendKeys(email);
+    }
 }
