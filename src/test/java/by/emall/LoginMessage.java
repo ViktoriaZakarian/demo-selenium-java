cc157011358b6067389f3907c5532f890fc62f4f
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 22:33:48 +0300

    add LoginMessage

---
+++ src/test/java/by/emall/LoginMessage.java
@@ -0,0 +1,9 @@
+package by.emall;
+
+public class LoginMessage {
+
+    public static final String ERROR_MESSAGE_NO_PHONE_NUMBER_NO_PASSWORD = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
+            "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String ERROR_MESSAGE_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String ERROR_MESSAGE_NO_PHONE_NUMBER = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
+}
