17fad1395e817c614aae1ae330610ebeebf42a6d
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 22:24:07 +0300

    add LoginXpath

---
+++ src/test/java/by/emall/LoginXpath.java
@@ -0,0 +1,8 @@
+package by.emall;
+
+public class LoginXpath {
+
+    public static final String BUTTON_ENTER_XPATH = "/html/body/div[1]/div[1]/div/div[1]/form/button";
+    public static final String INPUT_PHONE_NUMBER_XPATH = "/html/body/div[1]/div[1]/div/div[1]/form/div[1]/input";
+    public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div[1]/div/div[1]/form/div[2]/input";
+}
