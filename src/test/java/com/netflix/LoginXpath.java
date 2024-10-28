34fc44627cb21a78d9b8a8812e8f3cdead781f3c
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 18:10:34 +0300

    refactor LoginXpath

--- src/test/java/com/netflix/LoginXpath.java
+++ src/test/java/com/netflix/LoginXpath.java
@@ -1,4 +1,8 @@
 package com.netflix;
 
 public class LoginXpath {
+
+    public static final String INPUT_EMAIL_XPATH ="/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div/input";
+    public static final String INPUT_PASSWORD_XPATH ="/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div/input";
+    public static final String BUTTON_ENTRANCE_XPATH ="/html/body/div[1]/div/div/div[2]/div/form/button[1]";
 }
