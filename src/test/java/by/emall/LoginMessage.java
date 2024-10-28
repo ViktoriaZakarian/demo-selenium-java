87a7abb8a18cfd5ca0e2e1da2133c6184d8c956d
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 23:39:02 +0300

    refactor LoginMessage add ERROR_MESSAGE_INVALID_PHONE_NUMBER_OR_PASSWORD

--- src/test/java/by/emall/LoginMessage.java
+++ src/test/java/by/emall/LoginMessage.java
@@ -6,4 +6,5 @@ public class LoginMessage {
             "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_MESSAGE_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_MESSAGE_NO_PHONE_NUMBER = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
+    public static final String ERROR_MESSAGE_INVALID_PHONE_NUMBER_OR_PASSWORD = "Неверный логин или пароль";
 }
