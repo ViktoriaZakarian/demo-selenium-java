a0efa3af54b1e98c84cc246bf50cb4f5503a51a1
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 23:08:06 +0300

    refactor LoginMessage

--- src/test/java/by/emall/LoginMessage.java
+++ src/test/java/by/emall/LoginMessage.java
@@ -2,7 +2,7 @@ package by.emall;
 
 public class LoginMessage {
 
-    public static final String ERROR_MESSAGE_NO_PHONE_NUMBER_NO_PASSWORD = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
+    public static final String ERROR_MESSAGE_NO_PHONE_NUMBER_NO_PASSWORD = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)\n" +
             "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_MESSAGE_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_MESSAGE_NO_PHONE_NUMBER = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
