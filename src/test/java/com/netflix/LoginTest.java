b02e1d41fedab10075f97e13edbcf9e1446ca9db
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 21:24:15 +0300

    refactor LoginTest

--- src/test/java/com/netflix/LoginTest.java
+++ src/test/java/com/netflix/LoginTest.java
@@ -2,9 +2,7 @@ package com.netflix;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
@@ -17,13 +15,9 @@ public class LoginTest {
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.clickButtonEntrance();
 
-        String actualEmail = loginPage.getValidationMessageEmail();
-        String exceptedEmail = LoginMessage.INVALID_EMAIL;
-        Assertions.assertEquals(exceptedEmail, actualEmail);
+        Assertions.assertEquals(LoginMessage.INVALID_EMAIL, loginPage.getValidationMessageEmail());
 
-        String actualPassword = loginPage.getValidationMessagePassword();
-        String exceptedPassword = LoginMessage.INVALID_PASSWORD;
-        Assertions.assertEquals(actualPassword, exceptedPassword);
+        Assertions.assertEquals(LoginMessage.INVALID_PASSWORD, loginPage.getValidationMessagePassword());
     }
 
     @Test
@@ -35,9 +29,7 @@ public class LoginTest {
         loginPage.sendKeysInputEmail("zakarianviktoria@gmail.com");
         loginPage.clickButtonEntrance();
 
-        String actualPassword = loginPage.getValidationMessagePassword();
-        String exceptedPassword = LoginMessage.INVALID_PASSWORD;
-        Assertions.assertEquals(exceptedPassword, actualPassword);
+        Assertions.assertEquals(LoginMessage.INVALID_PASSWORD, loginPage.getValidationMessagePassword());
     }
 
     @Test
@@ -49,9 +41,7 @@ public class LoginTest {
         loginPage.sendKeysInputPassword("12345");
         loginPage.clickButtonEntrance();
 
-        String actualEmail = loginPage.getValidationMessageEmail();
-        String exceptedEmail = LoginMessage.INVALID_EMAIL;
-        Assertions.assertEquals(exceptedEmail, actualEmail);
+        Assertions.assertEquals(LoginMessage.INVALID_EMAIL, loginPage.getValidationMessageEmail());
     }
 
     @Test
@@ -64,8 +54,6 @@ public class LoginTest {
         loginPage.sendKeysInputPassword("12345");
         loginPage.clickButtonEntrance();
 
-        String actualMessage = loginPage.getErrorMessage();
-        String exceptedMessage = LoginMessage.ERROR_MESSAGE;
-        Assertions.assertEquals(exceptedMessage, actualMessage);
+        Assertions.assertEquals(LoginMessage.ERROR_MESSAGE, loginPage.getErrorMessage());
     }
 }
