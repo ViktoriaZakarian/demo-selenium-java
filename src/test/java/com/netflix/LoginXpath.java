05fe81f8ae9d6af7dc6f0035e18492423a17ab96
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 21:12:26 +0300

    refactor LoginXpath add ERROR_MESSAGE

--- src/test/java/com/netflix/LoginXpath.java
+++ src/test/java/com/netflix/LoginXpath.java
@@ -7,4 +7,5 @@ public class LoginXpath {
     public static final String BUTTON_ENTRANCE_XPATH ="/html/body/div[1]/div/div/div[2]/div/form/button[1]";
     public static final String VALIDATION_MESSAGE_EMAIL = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
     public static final String VALIDATION_MESSAGE_PASSWORD = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
+    public static final String ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/header/div/div/div";
 }
