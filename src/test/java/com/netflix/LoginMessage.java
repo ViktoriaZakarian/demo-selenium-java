29480eb56eaef30155da355045e7d421ee72bcda
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-28 21:10:51 +0300

    refactor LoginMessage add ERROR_MESSAGE

--- src/test/java/com/netflix/LoginMessage.java
+++ src/test/java/com/netflix/LoginMessage.java
@@ -3,4 +3,6 @@ package com.netflix;
 public class LoginMessage {
     public static final String INVALID_EMAIL = "Укажите действительный адрес электронной почты или номер телефона.";
     public static final String INVALID_PASSWORD = "Пароль должен содержать от 4 до 60 символов.";
+    public static final String ERROR_MESSAGE = "Неверный пароль для zakarianviktoria@gmail.com\n" +
+            "Вы можете использовать код для входа, сбросить пароль или повторить попытку.";
 }
