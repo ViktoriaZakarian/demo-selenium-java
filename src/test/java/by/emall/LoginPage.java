28266d76147fa56140bd99a130aa574f623528f7
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-30 18:43:22 +0300

    refactor LoginPage

--- src/test/java/by/emall/LoginPage.java
+++ src/test/java/by/emall/LoginPage.java
@@ -30,31 +30,26 @@ public class LoginPage {
     }
 
     public String getErrorMessageNoPhoneNumberNoPassword() {
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement errorMessageNoPhoneNumberNoPasswordWebElement = webDriver.findElement(By.xpath(LoginXpath.ERROR_MESSAGE_NO_PHONE_NUMBER_NO_PASSWORD_XPATH));
         return errorMessageNoPhoneNumberNoPasswordWebElement.getText();
     }
 
     public String getErrorMessageNoPhoneNumber() {
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement errorMessageNoPhoneNumberWebElement = webDriver.findElement(By.xpath(LoginXpath.ERROR_MESSAGE_NO_PHONE_NUMBER_XPATH));
         return errorMessageNoPhoneNumberWebElement.getText();
     }
 
     public String getErrorMessageNoPassword() {
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement errorMessageNoPasswordWebElement = webDriver.findElement(By.xpath(LoginXpath.ERROR_MESSAGE_NO_PASSWORD_XPATH));
         return errorMessageNoPasswordWebElement.getText();
     }
 
     public String getErrorMessageInvalidPhoneNumberOrPassword() {
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement errorMessageInvalidPhoneNumberOrPasswordWebElement = webDriver.findElement(By.xpath(LoginXpath.ERROR_MESSAGE_INVALID_PHONE_NUMBER_OR_PASSWORD_XPATH));
         return errorMessageInvalidPhoneNumberOrPasswordWebElement.getText();
     }
 
     public void clickButtonAcceptAllCookies() {
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement buttonAcceptAllCookiesWebElement = webDriver.findElement(By.xpath(LoginXpath.BUTTON_ACCEPT_ALL_COOKIES_XPATH));
         buttonAcceptAllCookiesWebElement.click();
     }
