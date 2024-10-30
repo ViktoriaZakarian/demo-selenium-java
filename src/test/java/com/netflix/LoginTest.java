c1610aa541097754081a8c37de335dd93fc18e5c
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-30 18:14:35 +0300

    refactor LoginTest add BeforeEach and AfterEach

--- src/test/java/com/netflix/LoginTest.java
+++ src/test/java/com/netflix/LoginTest.java
@@ -1,31 +1,38 @@
 package com.netflix;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class LoginTest {
+
+    WebDriver webDriver;
+    LoginPage loginPage;
+
+    @BeforeEach
+    public void setup() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        webDriver.get("https://www.netflix.com/login");
+
+        loginPage = new LoginPage(webDriver);
+    }
 
     @Test
     public void test1() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.netflix.com/login");
-
-        LoginPage loginPage = new LoginPage(webDriver);
         loginPage.clickButtonEntrance();
 
         Assertions.assertEquals(LoginMessage.INVALID_EMAIL, loginPage.getValidationMessageEmail());
-
         Assertions.assertEquals(LoginMessage.INVALID_PASSWORD, loginPage.getValidationMessagePassword());
     }
 
     @Test
     public void test2() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.netflix.com/login");
-
-        LoginPage loginPage = new LoginPage(webDriver);
         loginPage.sendKeysInputEmail("zakarianviktoria@gmail.com");
         loginPage.clickButtonEntrance();
 
@@ -34,10 +41,6 @@ public class LoginTest {
 
     @Test
     public void test3() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.netflix.com/login");
-
-        LoginPage loginPage = new LoginPage(webDriver);
         loginPage.sendKeysInputPassword("12345");
         loginPage.clickButtonEntrance();
 
@@ -46,14 +49,15 @@ public class LoginTest {
 
     @Test
     public void test4() {
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.netflix.com/login");
-
-        LoginPage loginPage = new LoginPage(webDriver);
         loginPage.sendKeysInputEmail("zakarianviktoria@gmail.com");
         loginPage.sendKeysInputPassword("12345");
         loginPage.clickButtonEntrance();
 
         Assertions.assertEquals(LoginMessage.ERROR_MESSAGE, loginPage.getErrorMessage());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        webDriver.quit();
     }
 }
