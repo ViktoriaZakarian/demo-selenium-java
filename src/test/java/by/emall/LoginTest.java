5cd1e33b19b71fb37c8e68d667468a4a7171573e
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-30 20:10:26 +0300

    refactor LoginTest add BeforeEach and AfterEach

--- src/test/java/by/emall/LoginTest.java
+++ src/test/java/by/emall/LoginTest.java
@@ -1,61 +1,66 @@
 package by.emall;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class LoginTest {
+
+    WebDriver webdriver;
+    LoginPage loginPage;
+
+    @BeforeEach
+    public void setup() {
+        webdriver = new ChromeDriver();
+        webdriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        webdriver.get("https://emall.by/login/password");
+
+        loginPage = new LoginPage(webdriver);
+    }
 
     @Test
     public void test1() {
-        WebDriver webdriver = new ChromeDriver();
-        webdriver.get("https://emall.by/login/password");
-
-        LoginPage loginPage = new LoginPage(webdriver);
-        loginPage.clickButtonEnter();
         loginPage.clickButtonAcceptAllCookies();
+        loginPage.clickButtonEnter();
 
-        Assertions.assertEquals(LoginMessage.ERROR_MESSAGE_NO_PHONE_NUMBER_NO_PASSWORD, loginPage.getErrorMessageNoPhoneNumberNoPassword());
+        Assertions.assertEquals(LoginMessage.THE_PHONE_NUMBER_FIELD_IS_REQUIRED_THE_PASSWORD_FIELD_IS_REQUIRED, loginPage.getErrorMessageNoPhoneNumberNoPassword());
     }
 
     @Test
     public void test2() {
-        WebDriver webdriver = new ChromeDriver();
-        webdriver.get("https://emall.by/login/password");
-
-        LoginPage loginPage = new LoginPage(webdriver);
         loginPage.sendKeysInputPhoneNumber("255756476");
         loginPage.clickButtonAcceptAllCookies();
         loginPage.clickButtonEnter();
 
-        Assertions.assertEquals(LoginMessage.ERROR_MESSAGE_NO_PASSWORD, loginPage.getErrorMessageNoPassword());
+        Assertions.assertEquals(LoginMessage.THE_PASSWORD_FIELD_IS_REQUIRED, loginPage.getErrorMessageNoPassword());
     }
 
     @Test
     public void test3() {
-        WebDriver webdriver = new ChromeDriver();
-        webdriver.get("https://emall.by/login/password");
-
-        LoginPage loginPage = new LoginPage(webdriver);
         loginPage.sendKeysInputPassword("dcvcv");
         loginPage.clickButtonAcceptAllCookies();
         loginPage.clickButtonEnter();
 
-        Assertions.assertEquals(LoginMessage.ERROR_MESSAGE_NO_PHONE_NUMBER, loginPage.getErrorMessageNoPhoneNumber());
+        Assertions.assertEquals(LoginMessage.THE_PHONE_NUMBER_FIELD_IS_REQUIRED, loginPage.getErrorMessageNoPhoneNumber());
     }
 
     @Test
     public void test4() {
-        WebDriver webdriver = new ChromeDriver();
-        webdriver.get("https://emall.by/login/password");
-
-        LoginPage loginPage = new LoginPage(webdriver);
         loginPage.sendKeysInputPhoneNumber("255756476");
         loginPage.sendKeysInputPassword("dcvcv");
         loginPage.clickButtonAcceptAllCookies();
         loginPage.clickButtonEnter();
 
-        Assertions.assertEquals(LoginMessage.ERROR_MESSAGE_INVALID_PHONE_NUMBER_OR_PASSWORD, loginPage.getErrorMessageInvalidPhoneNumberOrPassword());
+        Assertions.assertEquals(LoginMessage.INVALID_LOGIN_OR_PASSWORD, loginPage.getErrorMessageInvalidPhoneNumberOrPassword());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        webdriver.quit();
     }
 }
