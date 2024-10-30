ec921673b867db686843a2a39b0f64a8d1d6e4e6
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-30 18:42:59 +0300

    refactor LoginMessage rename Strings

--- src/test/java/by/emall/LoginMessage.java
+++ src/test/java/by/emall/LoginMessage.java
@@ -2,9 +2,9 @@ package by.emall;
 
 public class LoginMessage {
 
-    public static final String ERROR_MESSAGE_NO_PHONE_NUMBER_NO_PASSWORD = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
+    public static final String THE_PHONE_NUMBER_FIELD_IS_REQUIRED_THE_PASSWORD_FIELD_IS_REQUIRED = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
             "Поле \"Пароль\" обязательно для заполнения.";
-    public static final String ERROR_MESSAGE_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
-    public static final String ERROR_MESSAGE_NO_PHONE_NUMBER = "Поле \"Номер телефона\" обязательно для заполнения.";
-    public static final String ERROR_MESSAGE_INVALID_PHONE_NUMBER_OR_PASSWORD = "Неверный логин или пароль";
+    public static final String THE_PASSWORD_FIELD_IS_REQUIRED = "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String THE_PHONE_NUMBER_FIELD_IS_REQUIRED = "Поле \"Номер телефона\" обязательно для заполнения.";
+    public static final String INVALID_LOGIN_OR_PASSWORD = "Неверный логин или пароль";
 }
